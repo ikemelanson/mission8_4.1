4e4ff6f0578bf74ada0026088ea37404e51c1446
Devon Wolsleger <dwolsleg@byu.edu>
2023-02-22 13:21:59 -0700

    Adding the models

---
+++ mission8_4.1/Models/Context.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission8_4._1.Models
+{
+    public class Context : Controller
+    {
+
+
+        //Seed the data 
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Home" },
+                new Category { CategoryId = 2, CategoryName = "School" },
+                new Category { CategoryId = 3, CategoryName = "Work" },
+                new Category { CategoryId = 4, CategoryName = "Church" });
+
+            mb.Entity<Forum>().HasData(
+
+                    new Forum
+                    {
+                        TaskId = 1,
+                        CategoryId = 1,
+                        Task = "Clean Car",
+                        DueDate = 3 - 10 - 2023,
+                        Quadrant = "Important Urgent",
+                        Completed = false,
+                    }
+                );
+        }
+    }
+}
+
---
+++ mission8_4.1/Models/Forum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission8_4._1.Models
+{
+    public class Forum
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        [Required]
+        public string Task { get; set; }
+        public int DueDate { get; set; }
+        [Required]
+        public string Quadrant { get; set; }
+        public bool Completed { get; set; }
+
+        //Building foreign key relationship
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
+
