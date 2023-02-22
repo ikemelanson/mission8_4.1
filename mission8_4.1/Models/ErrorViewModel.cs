df27711255fd3b6de2775227d3428cecc53fe7e6
Ike Melanson <isaacmel@byu.edu>
2023-02-22 12:39:04 -0700

    Add project files.

---
+++ mission8_4.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mission8_4._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
